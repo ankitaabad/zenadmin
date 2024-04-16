2adaf278b9078c48b7572045fe9bed78d5b5f5d4
ankit <ankitaabad.1@gmail.com>
2024-04-16 22:03:18 +0530

    test pre commit hook

--- src/apis/buyer/contract.ts
+++ src/apis/buyer/contract.ts
@@ -1,25 +1,27 @@
 import { initContract } from '@ts-rest/core';
 import { z } from 'zod';
-// @ts-ignore  
-import type * as s from "zapatos/schema"
+// @ts-ignore
+import type * as s from 'zapatos/schema';
 const c = initContract();
-export const buyerContract = c.router({
-  "getSellers": {
-    method: "GET",
-    path: "/list-of-sellers",
-    responses: {
-      200: c.type<{ id: string, username: string }[]>()
+export const buyerContract = c.router(
+  {
+    getSellers: {
+      method: 'GET',
+      path: '/list-of-sellers',
+      responses: {
+        200: c.type<{ id: string; username: string }[]>(),
+      },
+      summary: 'get all the sellers',
     },
-    summary: "get all the sellers"
+    getSellerCatalog: {
+      method: 'GET',
+      path: 'seller-catalog/:sellerId',
+      pathParams: z.object({ sellerId: z.string() }),
+      responses: {
+        200: c.type<s.Products.JSONSelectable[]>(),
+      },
+      summary: 'get list of products sold by a seller',
+    },
   },
-  "getSellerCatalog": {
-    method:"GET",
-    path:"seller-catalog/:sellerId",
-    pathParams: z.object({sellerId:z.string()}),
-    responses:{
-      200: c.type<s.Products.JSONSelectable[]>()
-    },
-    summary: "get list of products sold by a seller"
-  }
-
-}, { pathPrefix: "/api/buyer" });
+  { pathPrefix: '/api/buyer' }
+);
